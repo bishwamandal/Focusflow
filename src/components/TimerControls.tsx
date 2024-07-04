9cf1aecb36c00db1a3f50e1719c5ce7df7f28985
Bishwa Mandal <mandalbishwarup@gmail.com>
2024-07-04 19:16:04 +0530

    Update Code

--- src/components/TimerControls.tsx
+++ src/components/TimerControls.tsx
@@ -10,15 +10,15 @@ const TimerControls: React.FC<TimerControlsProps> = React.memo(({ onStart, onSto
   return (
     <div className="card-footer">
       <button className="btn-success" onClick={onStart}>
-        <img className="play" src="/public/icons/play.svg" alt="Play" />
+        <img className="play" src="/icons/play.svg" alt="Play" />
         Start
       </button>
       <button className="btn-danger" onClick={onStop}>
-        <img className="stop" src="/public/icons/pause.svg" alt="Stop" />
+        <img className="stop" src="/icons/pause.svg" alt="Stop" />
         Stop
       </button>
       <button className="btn-reset" onClick={onReset}>
-        <img className="reset" src="/public/icons/reset.svg" alt="Reset" />
+        <img className="reset" src="/icons/reset.svg" alt="Reset" />
         Reset
       </button>
     </div>
